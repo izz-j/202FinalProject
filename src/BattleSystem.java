96e8cd28d5c8f1bba937e6b6b39da7f79a013134
HK651ScarletEK <58537296+HK651ScarletEK@users.noreply.github.com>
2019-12-11 15:39:33 -0500

    Add files via upload
    Updated BattleSystem and Fighters java files - some methods added, comments

--- src/BattleSystem.java
+++ src/BattleSystem.java
@@ -1,4 +1,35 @@
 
-public class BattleSystem {
+public class BattleSystem 
+{
+	//Attributes
 
+	
+	
+	//Private Methods
+	private static void SaveFighter()
+	{
+		
+	}
+	
+	private static void LoadFighter()
+	{
+		
+	}
+	
+	private static void rivalSelector()
+	{
+		
+	}
+	
+	private static void fighterPrompt()
+	{
+		
+	}
+	
+	//Public Methods
+	public static void statGenerator()
+	{
+		
+	}
+	
 }
--- src/Fighters.java
+++ src/Fighters.java
@@ -1,4 +1,22 @@
 
-public class Fighters {
-
+public class Fighters
+{
+	//Attributes
+	
+	
+	//Methods
+	public static void getFighter()
+	{
+		
+	}
+	
+	public void Getters()
+	{
+		
+	}
+	
+	public void Setters()
+	{
+		
+	}
 }
