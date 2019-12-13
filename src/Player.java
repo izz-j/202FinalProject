0b3e1d0134d19167175cc5fb574fbc2e011d0421
Iseman Johnson <izz@debianlaptop.local>
2019-12-13 15:18:34 -0500

    Added a player class. Will be used when time for battle. It inherits from fighter. Feel free to add stuff to it

---
+++ src/Player.java
@@ -0,0 +1,11 @@
+
+public class Player extends Fighters{
+//There will only be one instance of this class
+//Thisclass will be used in combat
+	Player (){
+		this.name = "player";
+		this.health = 0;
+		this.strength = 0;
+		this.specialMove = 0;
+	}
+}
