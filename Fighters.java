cf546075cce38d51033536e791a7941f63a1f5e4
HK651ScarletEK <58537296+HK651ScarletEK@users.noreply.github.com>
2019-12-14 21:08:11 -0500

    Further changes to LoadFighters and Main class
    implemented LoadFighters in BattleSystem to call Player and Rival classes, Rival class implemented

---
+++ Fighters.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+/*
+ * Fighters should be called just Fighter. Because only ONE fighter will be created once instantiated 
+ */
+public class Fighters 
+{
+	//Attributes
+	//fighter template
+	public String name;
+	public int health;
+	public int strength;
+	public int specialMove;
+	Fighters(){
+		name = "player";
+		health = 100;
+		strength = 10;
+	}
+	//Methods
+	
+	public static void getFighter()
+	{
+		
+	}
+	
+	public void Getters()
+	{
+		
+	}
+	
+	public void Setters()
+	{
+		
+	}
+}
---
+++ Player.java
@@ -0,0 +1,11 @@
+
+public class Player extends Fighters {
+//There will only be one instance of this class
+//This class will be used in combat
+	Player (){
+		this.name = "player";
+		this.health = 0;
+		this.strength = 0;
+		this.specialMove = 0;
+	}
+}
